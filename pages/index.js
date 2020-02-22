d91a6f6b50c0dc99bf15019cee1dc466339a5f83
Sebastian Wilgosz <sebastian@driggl.com>
2020-02-22 22:24:20 +0100

    Test og meta image in the same domain

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
     <div>
       <Head
         title="Driggl title"
-        ogImage="https://driggl-prod.s3.eu-central-1.amazonaws.com/suggester/blogs/99a95609-d8fd-4a84-994c-2386fd1c15de/uploads/3792710c-94ac-4173-98fd-5e85e061f2f9/sharing.jpeg"
+        ogImage="/home-cover.jpg"
       />
       {/* <Nav /> */}
 
